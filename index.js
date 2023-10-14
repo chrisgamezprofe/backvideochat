7346254a3aab195ae432e57ab961d5f39054b869
Christian Gámez <cgamez@atlasdev.uy>
2023-10-14 15:21:40 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const https = require("https")
+const app = express()
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const options = {
+    key: fs.readFileSync("localhost-key.pem"),
+    cert: fs.readFileSync("localhost.pem")
+};
+const server = https.createServer(options,app)
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        method:["GET","POST"]
+    }
+});
+
+io.on("connection",(socket)=> {
+    console.log(`user connected ${socket.id}`)
+
+    socket.emit("user", uuidv4());
+    
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded")
+    })
+
+    socket.on("callUser", (data) => {
+        io.to(data.userToCall).emit("callUser", {
+            signal: data.signalData,
+            name: data.name,
+            from: data.from
+      });
+    });
+
+    socket.on("answerCall", (data) => {
+      io.to(data.to).emit("callAccepted", data.signal);
+    });
+
+
+})
+
+
+server.listen(5000, () => {
+    console.log("Servidor corriendo en 5000")
+})
