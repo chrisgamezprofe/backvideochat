f1a1bfe7c494e9a0e6d79e6d211d37abe6fce520
Christian Gámez <cgamez@atlasdev.uy>
2023-10-15 09:31:05 -0500

    5 commit

--- index.js
+++ index.js
@@ -1,45 +1,32 @@
-const express = require("express")
-const http = require("http")
-const app = express()
-const server = http.createServer(app)
-
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://frontvideochat.vercel.app",
-    method: ["GET", "POST"]
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"]
   }
 });
 
-app.get("/", (req, res) => {
-  res.send("Corriendo");
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name
+    });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", {signal:data.signal,name:data.name});
+  });
 });
 
-io.on("connection",(socket)=> {
-    console.log(`user connected ${socket.id}`)
-
-    socket.emit("user", socket.id);
-    
-    socket.on("disconnect", () => {
-        socket.broadcast.emit("callEnded")
-    })
-
-    socket.on("callUser", (data) => {
-        io.to(data.userToCall).emit("callUser", {
-            signal: data.signalData,
-            name: data.name,
-            from: data.from
-      });
-    });
-
-    socket.on("answerCall", (data) => {
-      io.to(data.to).emit("callAccepted", data.signal);
-    });
-
-
-})
-
-const PORT = 3000 
-
-server.listen(PORT || 3000, () => {
-    console.log("Servidor corriendo en 5000")
-})
+server.listen(5000, () => console.log("server is running on port 5000"));
